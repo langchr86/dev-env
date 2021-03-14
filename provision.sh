619c24f427314dcf24b878130e57cebba9ce3574
Christian Lang <lang.chr86@gmail.com>
2021-03-14 17:05:25 +0100

    ! explicitly used python3 for ansible execution

--- provision.sh
+++ provision.sh
@@ -12,4 +12,5 @@ ansible-playbook \
   --connection=local \
   --inventory 127.0.0.1, \
   --limit 127.0.0.1 \
+  --extra-vars 'ansible_python_interpreter=/usr/bin/python3' \
   ${SCRIPT_DIR}/ansible/playbook.yml
