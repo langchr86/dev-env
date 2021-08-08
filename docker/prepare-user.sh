3018bdcc2cf229ab3e04d1602a7b4fbc27320a37
Christian Lang <lang.chr86@gmail.com>
2021-08-08 17:41:09 +0200

    ! define known password for user in docker

--- docker/prepare-user.sh
+++ docker/prepare-user.sh
@@ -8,5 +8,8 @@ CUSTOM_GID=${4:-1000}
 groupadd -o -g ${CUSTOM_GID} ${GROUP}
 useradd --no-log-init -d "/home/${USER}" -u ${CUSTOM_UID} -g ${CUSTOM_GID} -m -s /bin/bash ${USER}
 
+# define password
+echo -e "${USER}\n${USER}" | passwd ${USER}
+
 # make sudo not use a password
 echo "${USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
